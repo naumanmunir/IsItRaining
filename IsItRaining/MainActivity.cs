11854fe3069ff6dea82cf51efe97234d7757a1bc
nonabona <bloodo9@hotmail.com>
2017-05-07 22:45:29 -0400

    First ever!

--- IsItRaining/MainActivity.cs
+++ IsItRaining/MainActivity.cs
@@ -1,18 +1,218 @@
-﻿using Android.App;
-using Android.Widget;
+﻿using System;
+using Android.App;
+using Android.Locations;
 using Android.OS;
+using Android.Widget;
+using System.Collections.Generic;
+using System.Linq;
+using Android.Runtime;
+using System.Threading.Tasks;
+using System.Text;
+using OpenWeatherMap;
+using DarkSkyApi;
+using ME.Grantland.Widget;
 
 namespace IsItRaining
 {
-    [Activity(Label = "IsItRaining", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "Is It Raining?", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/MyTheme")]
+    public class MainActivity : Activity, ILocationListener
     {
+        //static readonly string TAG = "X:" + typeof(Activity1).Name;
+        TextView _addressText;
+        static Android.Locations.Location _currentLocation;
+        static LocationManager _locationManager;
+        
+        string _locationProvider;
+
+        TextView weatherStatus;
+        TextView rainingOrNot;
+        TextView currentTempurature;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            
+
+            _addressText = FindViewById<TextView>(Resource.Id.address_text);
+
+            weatherStatus = FindViewById<TextView>(Resource.Id.weatherSummary_text);
+            rainingOrNot = FindViewById<TextView>(Resource.Id.status);
+            currentTempurature = FindViewById<TextView>(Resource.Id.tempurature_text);
+
+            //FindViewById<TextView>(Resource.Id.get_address_button).Click += AddressButton_OnClick;
+
+
+            InitializeLocationManager();
+
+        }
+
+        private void InitializeLocationManager()
+        {
+            _locationManager = (LocationManager)GetSystemService(LocationService);
+
+            Criteria criteriaForLocationService = new Criteria
+            {
+                Accuracy = Android.Locations.Accuracy.Fine
+            };
+
+            IList<string> acceptableLocationProviders = _locationManager.GetProviders(criteriaForLocationService, true);
+
+            if (acceptableLocationProviders.Any())
+            {
+                _locationProvider = acceptableLocationProviders.First();
+
+
+                //_locationManager.GetLastKnownLocation("gps");
+                //_locationManager.GetLastKnownLocation("network");
+
+                _locationManager.RequestLocationUpdates(LocationManager.GpsProvider, 0, 0, this);
+                _locationManager.RequestLocationUpdates(LocationManager.NetworkProvider, 0, 0, this);
+
+                if (_currentLocation == null)
+                {
+                    //error
+                    System.Diagnostics.Debug.WriteLine("No Location found");
+                }
+                else
+                {
+                    
+                }
+            }
+            else
+            {
+                _locationProvider = string.Empty;
+
+                //LocationSettingsRequest
+            }
+
+        }
+
+        public void OnLocationChanged(Android.Locations.Location location)
+        {
+            _currentLocation = location;
+
+            if (_currentLocation == null)
+            {
+                
+            }
+            else
+            {
+                GetCurrentWeather(_currentLocation.Latitude, _currentLocation.Longitude);
+            }
+        }
+
+        private void DisplayAddress(Address address)
+        {
+            if (address != null)
+            {
+                StringBuilder deviceAddress = new StringBuilder();
+                for (int i = 0; i < address.MaxAddressLineIndex; i++)
+                {
+                    deviceAddress.AppendLine(address.GetAddressLine(i));
+                }
+                // Remove the last comma from the end of the address.
+                //_addressText.Text = deviceAddress.ToString();
+            }
+            else
+            {
+                _addressText.Text = "Unable to determine the address. Try again in a few minutes.";
+            }
+        }
+
+        //private async Task<Address> ReverseGeocodeCurrentLocation()
+        //{
+        //    Geocoder geocoder = new Geocoder(this);
+        //    IList<Address> addressList =
+        //        await geocoder.GetFromLocationAsync(_currentLocation.Latitude, _currentLocation.Longitude, 10);
+
+        //    Address address = addressList.FirstOrDefault();
+        //    return address;
+        //}
+
+        private void GetCurrentWeather(double latitde, double longitude)
+        {
+            DarkSkyService dk = new DarkSkyService("689a0d61985dfd4dae9cb86f67dcad79");
+
+            var request = dk.GetWeatherDataAsync(latitde, longitude);
+
+            SetStatus(request.Result.Hourly.Icon);
+
+            weatherStatus.Text = request.Result.Hourly.Summary;
+
+            if (request.Result.TimeZone.Contains("America"))
+            {
+                currentTempurature.Text = Math.Round(request.Result.Currently.ApparentTemperature).ToString() + " \u2109";
+            }
+            else
+            {
+                var toCelsius = (Math.Round(request.Result.Currently.ApparentTemperature) - 32) * (5 / 9);
+                currentTempurature.Text = toCelsius.ToString() + " \u2103";
+            }
+            
+
+            //request.Result.
+        }
+
+        private void DisplayAccurateImage(string summary)
+        {
+            var currentTime = DateTime.Now;
+        }
+
+        private void SetStatus(string summary)
+        {
+            if (summary.Contains("rain"))
+            {
+                rainingOrNot.Text = "yep";
+            }
+            else
+            {
+                rainingOrNot.Text = "nahh";
+            }
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            _locationManager.RequestLocationUpdates(_locationProvider, 0, 0, this);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _locationManager.RemoveUpdates(this);
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
+        {
+            //throw new NotImplementedException();
+        }
+
+        async void AddressButton_OnClick(object sender, EventArgs eventArgs)
+        {
+            if (_currentLocation == null)
+            {
+                _addressText.Text = "Can't determine the current address. Try again in a few minutes.";
+                return;
+            }
+            else
+            {
+
+            }
+            //Address address = await ReverseGeocodeCurrentLocation();
+            //DisplayAddress(address);
         }
     }
 }
--- IsItRaining/Resources/Resource.Designer.cs
+++ IsItRaining/Resources/Resource.Designer.cs
@@ -31,6 +31,15 @@ namespace IsItRaining
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010000
+			public const int minTextSize = 2130771968;
+			
+			// aapt resource value: 0x7f010001
+			public const int precision = 2130771969;
+			
+			// aapt resource value: 0x7f010002
+			public const int sizeToFit = 2130771970;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -45,7 +54,16 @@ namespace IsItRaining
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int clear_day = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int cloudy_day = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int rain = 2130837507;
 			
 			static Drawable()
 			{
@@ -57,11 +75,48 @@ namespace IsItRaining
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f060006
+			public const int address_text = 2131099654;
+			
+			// aapt resource value: 0x7f060004
+			public const int comments_text = 2131099652;
+			
+			// aapt resource value: 0x7f060005
+			public const int get_address_button = 2131099653;
+			
+			// aapt resource value: 0x7f060001
+			public const int status = 2131099649;
+			
+			// aapt resource value: 0x7f060003
+			public const int tempurature_text = 2131099651;
+			
+			// aapt resource value: 0x7f060000
+			public const int title = 2131099648;
+			
+			// aapt resource value: 0x7f060002
+			public const int weatherSummary_text = 2131099650;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int GeoLocationTest = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
@@ -88,6 +143,52 @@ namespace IsItRaining
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int AppTheme = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int MyTheme = 2131034113;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] AutofitTextView = new int[] {
+					2130771968,
+					2130771969,
+					2130771970};
+			
+			// aapt resource value: 0
+			public const int AutofitTextView_minTextSize = 0;
+			
+			// aapt resource value: 1
+			public const int AutofitTextView_precision = 1;
+			
+			// aapt resource value: 2
+			public const int AutofitTextView_sizeToFit = 2;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
